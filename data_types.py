bfc77a41b635290fbf9b80c3b6cd2f68fe5a2d94
JustineLeeNZ <jlu@stac.school.nz>
2025-02-17 11:18:30 +1300

    Examples of different data types

---
+++ data_types.py
@@ -0,0 +1,12 @@
+# about data types
+
+age = input("How old are you")
+age = int(age)
+print(age+10)
+
+cost = input("Cost? ")
+cost = float(cost)
+print(f"The GST is {cost * 0.15}")
+
+cost = float(input("Cost? "))
+print(f"The GST is {cost * 0.15}")
