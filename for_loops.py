fe11083694354c30e07d1d359e79a137f233fd21
JustineLeeNZ <jlu@stac.school.nz>
2025-02-21 13:22:25 +1300

    for loop examples

---
+++ for_loops.py
@@ -0,0 +1,27 @@
+''' examples of for loops - Justine Lee '''
+'''
+# counts up
+for i in range(10):
+    print(i)
+
+#
+for i in range(11):
+    print(i)
+
+for i in range(10):
+    print(i+1)
+
+'''
+
+
+
+
+
+for i in range(1,11):
+    print(i)
+
+stop_value = int(input("Number? "))
+
+for i in range(1, stop_value+1):
+    print(i)
+
